3e02193b0ffb06a978e899e10134b9897f637f66
Adam Rutkowski <a_rutkowski@outlook.com>
2018-02-05 17:43:45 +0100

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(js|ts)$",
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json",
+    "node"
+  ]
+};
