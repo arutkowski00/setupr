b36567efd5d0e9da3715a7d8f3b2aedebaefb29e
Adam Rutkowski <a_rutkowski@outlook.com>
2018-02-20 22:32:48 +0100

    chore: move args to src/index

--- src/config/argv.config.ts
+++
@@ -1,9 +0,0 @@
-import * as yargs from 'yargs';
-
-export const argvConfig = yargs
-  .usage('Usage: $0 <cmd> [options]') // usage string of application.
-  .help().alias('help', 'h')
-  .showHelpOnFail(true)
-  .version()
-  .command('init <repository>', 'initialize project from repository')
-  .demandCommand();
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import 'reflect-metadata';
-import { argvConfig } from './config/argv.config';
-import { AppContainer } from './ioc';
+import * as yargs from 'yargs';
+import { AppContainer } from './ioc/container';
+import { Arguments } from './ioc/interfaces';
 
-const args = argvConfig.argv;
+const args: Arguments = yargs
+  .usage('Usage: $0 <cmd> [options]')
+  .help().alias('help', 'h')
+  .showHelpOnFail(true)
+  .version()
+  .command('init <repository>', 'initialize project from repository')
+  .demandCommand()
+  .argv;
+
 const container = new AppContainer(args);
 const command = container.getCommand(args._[0]);
 command.execute(args);
